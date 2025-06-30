209b70fda4cbb77bf52fb2363890bb411c98e499
Lluis Alsina Navarro <lalsina883@gmail.com>
2025-06-30 18:58:08 +0200

    Se añaden routes par aprobar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './components/main-component/main-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    /*
+    {path:'', pathMatch: 'full', redirectTo:'Inicio'},
+    {path:'Inicio', component: MainComponent},
+    {path:'**', redirectTo: 'inicio'} */
+];
